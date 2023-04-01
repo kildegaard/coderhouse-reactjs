77f4990fcd04b423020a721faeea2bc1cee4c787
Gus <gustavo.kildegaard@gmail.com>
2023-04-01 13:16:33 -0300

    fin ItemCount

--- react-coderhouse/src/components/ItemCount/ItemCount.jsx
+++ react-coderhouse/src/components/ItemCount/ItemCount.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './ItemCount.css';
 
 export const ItemCount = () => {
+
+    const [counter, setCounter] = useState(0);
+
+    const sumar = () => {
+        setCounter(counter + 1);
+    }
+    const restar = () => {
+        if (counter > 0) {
+            setCounter(counter - 1);
+        }
+    }
+    const reset = () => {
+        setCounter(0);
+    }
+
     return (
-        <>Soy un contador</>
+        <>
+            <h4>Cantidades: {counter}</h4>
+            <div className='counter-btns'>
+                <button onClick={sumar}>➕</button>
+                {counter === 0 ? <button disabled>➖</button> : <button onClick={restar}>➖</button>}
+                <button onClick={reset}>🔄</button>
+            </div>
+        </>
     )
 }
