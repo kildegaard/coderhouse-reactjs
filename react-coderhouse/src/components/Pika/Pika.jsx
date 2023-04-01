1b89f1c2aad7173baf22e63ce0ee7877dad18c35
Gus <gustavo.kildegaard@gmail.com>
2023-04-01 14:49:31 -0300

    fin clase 3

--- react-coderhouse/src/components/Pika/Pika.jsx
+++ react-coderhouse/src/components/Pika/Pika.jsx
@@ -19,14 +19,60 @@ export const Pika = () => {
                     img: data.sprites.front_default,
                 })
             })
-    }, [])
+    }, [id])
 
+    const anterior = () => {
+        id > 1 && setId(id - 1)
+    }
+
+    const siguiente = () => {
+        id < 1015 && setId(id + 1)
+    }
+
+    const buscar = (e) => {
+        setBusqueda(e.target.value)
+    }
+
+    const submit = (e) => {
+        e.preventDefault()
+        if (busqueda.length > 2) {
+            fetch(`https://pokeapi.co/api/v2/pokemon/${busqueda}`)
+                .then(res => res.json())
+                .then(data => {
+                    setPokemon({
+                        nombre: data.name,
+                        img: data.sprites.front_default,
+                    })
+                    setId(
+                        data.id
+                    )
+                })
+        }
+    }
     return (
         <>
-            <h3 className="pika">Hi, I'm Pika!</h3>
-            <h3 className="pika">{pokemon.nombre}</h3>
-            <img src={pokemon.img} alt="" />
-            <p>Holi</p>
+            <div>
+                {!pokemon ?
+                    <h1 className="poke">Cargando...</h1>
+                    :
+                    <div className="poke">
+                        <h3 className="pika">{pokemon.numero}</h3>
+                        <h3 className="pika">{pokemon.nombre}</h3>
+                        <img className="pika" src={pokemon.img} alt="" />
+                    </div>
+                }
+            </div>
+
+            <div className="poke">
+                <button onClick={anterior}>Anterior</button>
+                <button onClick={siguiente}>Siguiente</button>
+            </div>
+
+            <>
+                <form onSubmit={submit} className="poke">
+                    <input type="text" onChange={buscar} />
+                </form>
+            </>
         </>
     )
 }
