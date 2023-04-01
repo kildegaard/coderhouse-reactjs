6710b83fd835635423c3369d2b48e67e48acd292
Gus <gustavo.kildegaard@gmail.com>
2023-04-01 14:07:34 -0300

    Ya tenemos a bulbasaur!!

--- react-coderhouse/src/components/Pika/Pika.jsx
+++ react-coderhouse/src/components/Pika/Pika.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Pika.css";
-import { useState } from "react";
 
 export const Pika = () => {
 
     const [pokemon, setPokemon] = useState('')
 
+    const [id, setId] = useState(1)
+
+    const [busqueda, setBusqueda] = useState('')
+
+    useEffect(() => {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+            .then(res => res.json())
+            .then(data => {
+                setPokemon({
+                    numero: data.id,
+                    nombre: data.name,
+                    img: data.sprites.front_default,
+                })
+            })
+    }, [])
 
     return (
-        <h3 className="pika">Hi, I'm Pika!</h3>
+        <>
+            <h3 className="pika">Hi, I'm Pika!</h3>
+            <h3 className="pika">{pokemon.nombre}</h3>
+            <img src={pokemon.img} alt="" />
+            <p>Holi</p>
+        </>
     )
 }
