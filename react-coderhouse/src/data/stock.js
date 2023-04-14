fbcfbf8f00efc20c939fe2d8d9a2e9216cc7b147
Gus <gustavo.kildegaard@gmail.com>
2023-04-14 18:11:41 -0300

    clase 4 - agrego data

---
+++ react-coderhouse/src/data/stock.js
@@ -0,0 +1,38 @@
+const stock = [
+    {
+        id: 1,
+        description: 'Pantalón',
+        price: 35,
+        image: 'https://picsum.photos/200/300'
+    },
+    {
+        id: 2,
+        description: 'Televisor',
+        price: 352,
+        image: 'https://picsum.photos/id/237/200/300'
+    },
+    {
+        id: 3,
+        description: 'Romero',
+        price: 200,
+        image: 'https://picsum.photos/200/300?grayscale'
+    },
+    {
+        id: 4,
+        description: 'Remera',
+        price: 35,
+        image: 'https://picsum.photos/200/300?blur'
+    },
+    {
+        id: 5,
+        description: 'Celular',
+        price: 352,
+        image: 'https://picsum.photos/id/237/200/300?grayscale'
+    },
+    {
+        id: 6,
+        description: 'Microondas',
+        price: 200,
+        mage: 'https://picsum.photos/200/300?grayscale&blur'
+    }
+]
