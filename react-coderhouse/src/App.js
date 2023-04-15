5d0f69eecf346e1ec261cb148005bf12f1d32bdb
Gus <gustavo.kildegaard@gmail.com>
2023-04-15 11:52:26 -0300

    borre el componente container que no era nada util

--- react-coderhouse/src/App.js
+++ react-coderhouse/src/App.js
@@ -1,7 +1,7 @@
 import { ItemCount } from './components/ItemCount/ItemCount';
 import { ItemListContainer } from './components/ItemListContainer/ItemListContainer';
 import { NavBar } from './components/NavBar/NavBar';
-import { Container } from './components/Container/Container';
+import { ItemDetailContainer } from './components/ItemDetailContainer/ItemDetailContainer';
 import { Pika } from './components/Pika/Pika';
 import {
   BrowserRouter as Router,
@@ -24,7 +24,7 @@ function App() {
         <Routes>
           <Route path='/' element={<ItemListContainer />} />
           <Route path='/productos/:category' element={<ItemListContainer />} />
-          <Route path='/detail/:itemId' element={<ItemDetailContainer />} />
+          {/* <Route path='/detail/:itemId' element={<ItemDetailContainer />} /> */}
           <Route path='/counter' element={<ItemCount />} />
           <Route path='/Pika' element={<Pika />} />
           <Route path='*' element={<Navigate to='/' />} /> // Esto es para que si no encuentra la ruta, redirija a la ruta principal
