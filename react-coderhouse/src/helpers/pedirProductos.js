89f9a0cf8a1cd5140bfa816854044bdef0cbc96f
Gus <gustavo.kildegaard@gmail.com>
2023-04-15 13:12:35 -0300

    le baje el timer a 500

--- react-coderhouse/src/helpers/pedirProductos.js
+++ react-coderhouse/src/helpers/pedirProductos.js
@@ -5,6 +5,6 @@ export const pedirProductos = () => {
         setTimeout(() => {
             resolve(stock)
             reject('Error al obtener los productos')
-        }, 2000)
+        }, 500)
     })
 }
