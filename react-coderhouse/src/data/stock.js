ad0e9c2b24d8d20bc59bb2e45ef50fbb3a2203c9
Gus <gustavo.kildegaard@gmail.com>
2023-04-15 11:43:05 -0300

    Agrego categorias a los productos

--- react-coderhouse/src/data/stock.js
+++ react-coderhouse/src/data/stock.js
@@ -3,36 +3,42 @@ export const stock = [
         id: 1,
         description: 'Pantalón',
         price: 35,
-        image: 'https://picsum.photos/200/300'
+        image: 'https://picsum.photos/200/300',
+        category: 'ropa'
     },
     {
         id: 2,
         description: 'Televisor',
         price: 352,
-        image: 'https://picsum.photos/id/237/200/300'
+        image: 'https://picsum.photos/id/237/200/300',
+        category: 'electrodoméstico'
     },
     {
         id: 3,
         description: 'Zapatilla',
         price: 200,
-        image: 'https://picsum.photos/200/300?grayscale'
+        image: 'https://picsum.photos/200/300?grayscale',
+        category: 'ropa'
     },
     {
         id: 4,
         description: 'Remera',
         price: 35,
-        image: 'https://picsum.photos/200/300?blur'
+        image: 'https://picsum.photos/200/300?blur',
+        category: 'ropa'
     },
     {
         id: 5,
         description: 'Celular',
         price: 352,
-        image: 'https://picsum.photos/id/237/200/300?grayscale'
+        image: 'https://picsum.photos/id/237/200/300?grayscale',
+        category: 'electrodoméstico'
     },
     {
         id: 6,
         description: 'Microondas',
         price: 200,
-        mage: 'https://picsum.photos/200/300?grayscale&blur'
+        mage: 'https://picsum.photos/200/300?grayscale&blur',
+        category: 'electrodoméstico'
     }
 ]
