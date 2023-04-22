a1b92cad1a44c63b2ce14f06ce42ee72c21efa48
Gus <gustavo.kildegaard@gmail.com>
2023-04-22 10:25:21 -0300

    modifique stock.js para agregar un nuevo key:value de stock

--- react-coderhouse/src/data/stock.js
+++ react-coderhouse/src/data/stock.js
@@ -4,41 +4,47 @@ export const stock = [
         description: 'Pantalón',
         price: 35,
         image: 'https://picsum.photos/200/200',
-        category: 'ropa'
+        category: 'ropa',
+        stock: 10
     },
     {
         id: 2,
         description: 'Televisor',
         price: 352,
         image: 'https://picsum.photos/id/237/200/200',
-        category: 'electrodomesticos'
+        category: 'electrodomesticos',
+        stock: 5
     },
     {
         id: 3,
         description: 'Zapatilla',
         price: 200,
         image: 'https://picsum.photos/200/200?grayscale',
-        category: 'ropa'
+        category: 'ropa',
+        stock: 2
     },
     {
         id: 4,
         description: 'Remera',
         price: 35,
         image: 'https://picsum.photos/200/200?blur',
-        category: 'ropa'
+        category: 'ropa',
+        stock: 0
     },
     {
         id: 5,
         description: 'Celular',
         price: 352,
         image: 'https://picsum.photos/id/236/200/200',
-        category: 'electrodomesticos'
+        category: 'electrodomesticos',
+        stock: 10
     },
     {
         id: 6,
         description: 'Microondas',
         price: 200,
         image: 'https://picsum.photos/200/200?grayscale&blur',
-        category: 'electrodomesticos'
+        category: 'electrodomesticos',
+        stock: 2
     }
 ]
